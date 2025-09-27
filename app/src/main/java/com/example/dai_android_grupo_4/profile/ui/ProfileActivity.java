f734a42e5c2c3f30a7a960fad4740c1ccab10128
Sofia Blasco <sofiblasco14@gmail.com>
2025-09-27 08:56:23 -0300

    Actualizacion

--- app/src/main/java/com/example/dai_android_grupo_4/profile/ui/ProfileActivity.java
+++ app/src/main/java/com/example/dai_android_grupo_4/profile/ui/ProfileActivity.java
@@ -49,7 +49,7 @@ public class ProfileActivity extends AppCompatActivity {
     private String token;
     private static final int PICK_IMAGE_REQUEST = 100;
     private Uri imageUri;
-    private static final String BASE_URL = "http://10.0.2.2:8080"; // Ajustar según tu backend
+    private static final String BASE_URL = "http://10.0.2.2:8080";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
